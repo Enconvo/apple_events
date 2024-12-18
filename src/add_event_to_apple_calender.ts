9a8e169badd149dee387327921c3f0cd5f91e3ef
ysnows <ysnows@163.com>
2024-12-18 20:09:29 +0800

    update

--- src/add_event_to_apple_calender.ts
+++ src/add_event_to_apple_calender.ts
@@ -1,26 +1,21 @@
-import { uuid, Action, ActionProps, AppleCalender, res, ChatHistory, LLMUtil, LLMProviderBase, ServiceProvider } from "@enconvo/api";
-import { HumanMessage, SystemMessage } from "@langchain/core/messages";
+import { Action, AppleCalender, res, SystemMessage, UserMessage, RequestOptions, EnconvoResponse, LLMProvider, ResponseAction } from "@enconvo/api";
 
+export default async function main(req: Request): Promise<EnconvoResponse> {
 
+  const options: RequestOptions = await req.json()
 
-export default async function main(req: Request) {
-
-  const { options } = await req.json()
-  const { input_text, context } = options
-
-  let content = input_text || context;
+  let content = options.input_text || options.selection_text || options.context;
 
   if (!content) {
     throw new Error("No text to process")
   }
 
-  const requestId = uuid()
 
   // format : 2021-01-01 10:00:00
   const nowTime = new Date().toLocaleString()
   const messages = [
     new SystemMessage(`As Apple's Calendar software`),
-    new HumanMessage(`Please extract the title and  startDate & endDate(format: (YYYY-MM-DD HH:MM:SS) like 2022-12-31 23:59:59) of Calendar Event from the following text.And Just return it to me in JSON format, No explanation needed!.
+    new UserMessage(`Please extract the title and  startDate & endDate(format: (YYYY-MM-DD HH:MM:SS) like 2022-12-31 23:59:59) of Calendar Event from the following text.And Just return it to me in JSON format, No explanation needed!.
 Example Input:
   Remind me tomorrow morning at 10 o'clock to go to Beijing for a meeting.
 
@@ -50,87 +45,46 @@ Output:
   ];
 
 
-  const resultID = uuid()
-
-  await res.write({
-    content: {
-      id: resultID,
-      role: "ai",
-      content: [
-        {
-          type: 'text',
-          text: "Analyzing the event ...",
-        }
-      ]
-    },
-    overwrite: true
-  })
+  res.writeLoading("Analyzing the event ...")
 
 
-  const chat: LLMProviderBase = ServiceProvider.load(options.llm)
+  const llmProvider = await LLMProvider.fromEnv()
+  const resultMessage = await llmProvider.call({ messages })
 
-  const stream = (await chat.call({ messages })).stream!
-  const llmResult = await LLMUtil.invokeLLM(stream)
-
-
-  let completion: string | undefined = llmResult;
+  let completion: string | undefined = resultMessage.text();
 
   completion = completion.replace(/```json/g, "")
   completion = completion.replace(/```/g, "")
-  // 只获取 {}json内容，如果有多余的文字，去掉
   completion = completion.match(/{.*}/s)?.[0];
 
   if (!completion) throw new Error("Invalid JSON format")
 
   const completionOBJ = JSON.parse(completion)
-  console.log("completionOBJ", completionOBJ)
-
 
   const result = await AppleCalender.addEvent({
     title: completionOBJ.title,
     startDate: completionOBJ.startDate,
     endDate: completionOBJ.endDate,
   })
-  console.log("result", result)
 
-  await ChatHistory.saveChatMessages({
-    input: content,
-    output: result,
-    llmOptions: options.llm,
-    messages,
-    requestId
-  });
 
-  const actions: ActionProps[] = [
+  const actions: ResponseAction[] = [
+    Action.OpenApplication({ app: "Calendar", title: "Open Calendar.app", icon: "calender.png" }),
     Action.Paste({
       content: result,
       closeMainWindow: true
     }),
-    Action.OpenApplication({ app: "Calendar", title: "Open Calendar.app", icon: "calender.png" }),
     Action.Copy({
       content: result,
       closeMainWindow: true
     })
   ]
 
-  const output = {
-    type: "messages",
-    messages: [
-      {
-        id: resultID,
-        role: "ai",
-        content: [
-          {
-            type: 'text',
-            text: result
-          }
-        ]
-      }
-    ],
+  return {
+    type: "text",
+    content: result,
     actions: actions
-  }
-
-  return output;
+  };
 
 
 }
--- src/add_event_to_apple_reminder.ts
+++ src/add_event_to_apple_reminder.ts
@@ -1,26 +1,21 @@
-import { uuid, ActionProps, Action, ChatHistory, res, AppleCalender, ServiceProvider, LLMProviderBase, LLMUtil } from "@enconvo/api";
-import { HumanMessage, SystemMessage } from "@langchain/core/messages";
+import { Action, res, AppleCalender, SystemMessage, UserMessage, LLMProvider, ResponseAction, RequestOptions, EnconvoResponse } from "@enconvo/api";
 
-export default async function main(req: Request) {
+export default async function main(req: Request): Promise<EnconvoResponse> {
 
-  const { options } = await req.json()
-  const { input_text, context, llm: llmOptions } = options
+  const options: RequestOptions = await req.json()
 
-  let content = input_text || context;
+  let content = options.input_text || options.selection_text || options.context;
 
   if (!content) {
     throw new Error("No text to process")
   }
-
-  const requestId = uuid()
-  // 如果translateText中有换行符，需要添加> 符号
 
   // format : 2021-01-01 10:00:00
   const nowTime = new Date().toLocaleString()
 
   const messages = [
     new SystemMessage(`As Apple's Reminder software`),
-    new HumanMessage(`Please extract the title and due date(format: (YYYY-MM-DD HH:MM:SS) like 2022-12-31 23:59:59) of Reminder from the following text.And Just return it to me in JSON format, No explanation needed!.
+    new UserMessage(`Please extract the title and due date(format: (YYYY-MM-DD HH:MM:SS) like 2022-12-31 23:59:59) of Reminder from the following text.And Just return it to me in JSON format, No explanation needed!.
 Example Input:
   Remind me tomorrow morning at 10 o'clock to go to Beijing for a meeting.
 
@@ -48,82 +43,45 @@ Output:
   ];
 
 
-  const resultID = uuid()
-
-  await res.write({
-    content: {
-      id: resultID,
-      role: "ai",
-      content: [
-        {
-          type: 'text',
-          text: "Analyzing the event ...",
-        }
-      ]
-    },
-    overwrite: true
-  })
+  res.writeLoading("Analyzing the event ...")
 
 
-  const chat: LLMProviderBase = ServiceProvider.load(llmOptions)
+  const llmProvider = await LLMProvider.fromEnv()
+  const llmResult = await llmProvider.call({ messages })
 
-  const stream = (await chat.call({ messages })).stream!
-  const llmResult = await LLMUtil.invokeLLM(stream)
-
-
-  let completion: string | undefined = llmResult;
+  let completion: string | undefined = llmResult.text();
 
   completion = completion.replace(/```json/g, "")
   completion = completion.replace(/```/g, "")
-  // 只获取 {}json内容，如果有多余的文字，去掉
   completion = completion.match(/{.*}/s)?.[0];
 
   if (!completion) throw new Error("Invalid JSON format")
 
   const completionOBJ = JSON.parse(completion)
-  console.log("completionOBJ", completionOBJ)
 
   const result = await AppleCalender.addReminder({
     title: completionOBJ.title,
     dueDate: completionOBJ.dueDate,
   })
 
-  await ChatHistory.saveChatMessages({
-    input: content,
-    output: result,
-    llmOptions: options.llm,
-    messages,
-    requestId
-  });
-  const actions: ActionProps[] = [
+
+  const actions: ResponseAction[] = [
+    Action.OpenApplication({ app: "Reminders", title: "Open Reminders.app", icon: "reminder.png" }),
     Action.Paste({
       content: result,
       closeMainWindow: true
     }),
-    Action.OpenApplication({ app: "Reminders", title: "Open Reminders.app", icon: "reminder.png" }),
     Action.Copy({
       content: result,
       closeMainWindow: true
     })
   ]
 
-  const output = {
-    type: "messages",
-    messages: [
-      {
-        id: resultID,
-        role: "ai",
-        content: [
-          {
-            type: 'text',
-            text: result
-          }
-        ]
-      }
-    ],
-    actions: actions
-  }
 
-  return output;
+  return {
+    type: "text",
+    content: result,
+    actions: actions
+  };
 
 }
